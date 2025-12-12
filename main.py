2c60041f6055e1b16cb6aa105eb8f8c9c9c06192
OvcaTV <mmatikm@gmail.com>
2025-12-12 01:21:19 +0100

    small change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 def emptyFileFinder(path, removeFolder):
@@ -5,15 +6,17 @@ def emptyFileFinder(path, removeFolder):
     for root, dirs, files in os.walk(path, topdown=False):
         if not dirs and not files:
             empty.append(root)
+            logging.info(f"Found empty file: {root}")
 
             if removeFolder is True:
                 try:
                     os.rmdir(root)
+                    logging.info(f"Deleted file: {root}")
                 except Exception as e:
                     print('Nelze smazat')
     return empty
 
-
+logging.basicConfig(filename="log.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", encoding="utf-8")
 
 if __name__ == "__main__":
     path = input('Zadej cestu pro hledani prazdnych slozek: ').strip()
@@ -25,7 +28,7 @@ if __name__ == "__main__":
     if not os.path.isdir(path):
         print("Neplatna cesta.")
     else:
-        findEmpty = emptyFileFinder(path, removeFolder)
+        findEmpty = emptyFileFinder(path, isRemoveFolder)
 
         print("\nPrazdne slozky:")
         for slozka in findEmpty:
