5d8c1ae495e2fc7de4419b53a7dbf88ec2249fa7
Martin M <156213340+OvcaTV@users.noreply.github.com>
2026-01-05 09:10:29 +0100

    Update Database-user.sql

---
+++ SQL Scripts/Database-user.sql
@@ -0,0 +1,8 @@
+CREATE LOGIN TestUser WITH PASSWORD = 'TestUser12!'; 
+CREATE USER TestUser FOR LOGIN TestUser; 
+
+ALTER ROLE db_datareader ADD MEMBER TestUser; 
+ALTER ROLE db_datawriter ADD MEMBER TestUser; 
+ALTER ROLE db_owner ADD MEMBER TestUser; 
+
+GRANT SELECT, INSERT, UPDATE, DELETE on SCHEMA::dbo TO TestUser;
