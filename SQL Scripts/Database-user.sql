6585b5d2c10494d05de2d1358b1eb006664bc2ae
Martin M <156213340+OvcaTV@users.noreply.github.com>
2026-01-05 09:11:17 +0100

    Update Database-user.sql

--- SQL Scripts/Database-user.sql
+++ SQL Scripts/Database-user.sql
@@ -1,4 +1,4 @@
-CREATE LOGIN TestUser WITH PASSWORD = 'TestUser12!'; 
+CREATE LOGIN TestUser WITH PASSWORD = 'TestUser123!'; 
 CREATE USER TestUser FOR LOGIN TestUser; 
 
 ALTER ROLE db_datareader ADD MEMBER TestUser; 
@@ -6,3 +6,4 @@ ALTER ROLE db_datawriter ADD MEMBER TestUser;
 ALTER ROLE db_owner ADD MEMBER TestUser; 
 
 GRANT SELECT, INSERT, UPDATE, DELETE on SCHEMA::dbo TO TestUser;
+
